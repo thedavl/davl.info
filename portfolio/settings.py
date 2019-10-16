3c27306e1bdb3f287e82dcfda70f3c062ddd80c1
David Lee <thedavl2001@gmail.com>
2019-10-15 21:22:22 -0700

    Updated settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,7 +138,7 @@ LOGOUT_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/dist')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
