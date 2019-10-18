58c3d573a3892a5bf5f584ea38a6a55c6da5f5a7
David Lee <thedavl2001@gmail.com>
2019-10-18 13:47:14 -0700

    updated home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,30 +3,6 @@
     <div class="container" style="padding: 40px" align="center">
       <h1 style="font-weight: bold">Hi, i'm David</h1>
       <h4 style="padding: 10px">And this is my Portfolio.</h4>
-      <!-- <div class="flex-container">
-        <div class="div-flex">
-          <br />
-          <h3 style="font-size: 25px">
-            Please visit the projects tab to see a bit of what i've done, and how this site was made
-            <br />
-          </h3>
-        </div>
-        <div class="divider"></div>
-        <div class="div-flex">
-          <br />
-          <h3 style="font-size: 25px">
-          The blog section of this site showcases features of the REST API i've built, and some functionalities i've implemented!</h3>
-        </div>
-      </div> -->
-      <!-- <div class="flex-container">
-        <div class="button-container">
-          <router-link :to="{ name: 'projects' }" class="btn btn-sm btn-primary">Projects</router-link>
-        </div>
-        <div class="divider"></div>
-        <div class="button-container">
-          <router-link :to="{ name: 'blog' }" class="btn btn-sm btn-info">Blog</router-link>
-        </div>
-      </div> -->
       
       <div class="flex-container">
         <div>
@@ -71,36 +47,32 @@ export default {
 .overlayL {
     font-size: 40px;
     color: white;
-    position: absolute; 
-    top: 370px;
-    left: 23%;
+    position: relative; 
+    bottom: 250px;
     text-decoration: none;
 }
 
 .overlayR {
     font-size: 40px;
     color: white;
-    position: absolute; 
-    top: 370px; 
-    right: 24%;
+    position: relative; 
+    bottom: 250px;
     text-decoration: none;
 }
 
 .overlayL-desc{
     font-size: 20px;
     color: white;
-    position: absolute; 
-    top: 440px;
-    left: 12%;
+    position: relative; 
+    bottom: 250px;
     text-decoration: none;
 }
 
 .overlayR-desc{
     font-size: 20px;
     color: white;
-    position: absolute; 
-    top: 440px; 
-    right: 10%;
+    position: relative; 
+    bottom: 250px;
     text-decoration: none;
 }
 
@@ -120,8 +92,6 @@ export default {
 
 .flex-container {
   display: flex;
-  align-items: stretch;
-  justify-content: center;
 }
 
 .image-container {
@@ -130,30 +100,8 @@ export default {
     padding: 10px;
 }
 
-.div-flex {
-  border-top-right-radius: 15px;
-  border-top-left-radius: 15px;
-  width: 500px;
-  height: 150px;
-  background-color: lightgrey;
-  padding: 10px 25px;
-}
-
-.button-container {
-  border-bottom-right-radius: 15px;
-  border-bottom-left-radius: 15px;
-  width: 500px;
-  height: 60px;
-  background-color: lightgrey;
-}
-
 .divider {
   width: 10px;
   background-color: white;
-}
-
-.div-right {
-  border-left: 10px;
-  border-color: black;
 }
 </style>
