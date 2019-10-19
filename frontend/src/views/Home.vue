c80dfab7656b4b1d4b841d8d824b23f99d33f6d7
David Lee <thedavl2001@gmail.com>
2019-10-18 17:04:12 -0700

    updated home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,16 +5,18 @@
       <h4 style="padding: 10px">And this is my Portfolio.</h4>
       
       <div class="flex-container">
-        <div>
-            <img class="image-container" style="filter: brightness(50%)" v-bind:src="coding" />
-            <router-link :to="{ name: 'projects' }" class="overlayL">Projects</router-link>
-            <p class="overlayL-desc">My projects, including how this site was built.</p>
+        <div class="hov">
+            <router-link :to="{ name: 'projects' }"><img class="image-container" v-bind:src="coding" /></router-link>
+            <router-link :to="{ name: 'projects' }" class="overlay">Projects</router-link>
+            <br>
+            <router-link :to="{ name: 'projects' }" class="overlay-desc">My projects, including how this site was built.</router-link>
         </div>
         <div class="divider"></div>
-        <div>
-            <img class="image-container" style="filter: brightness(40%)" v-bind:src="blog" />
-            <router-link :to="{ name: 'blog' }" class="overlayR">Blog</router-link>
-            <p class="overlayR-desc">Fully functioning blog, showing off the backend API</p>
+        <div class="hov">
+            <router-link :to="{ name: 'blog' }"><img class="image-container" v-bind:src="blog" /></router-link>
+            <router-link :to="{ name: 'blog' }" class="overlay">Blog</router-link>
+            <br>
+            <router-link :to="{ name: 'blog' }" class="overlay-desc">Fully functioning blog, built with a backend API</router-link>
         </div>
       </div>
     </div>
@@ -44,7 +46,11 @@ export default {
 
 <style scoped>
 
-.overlayL {
+.hov:hover .image-container {
+    filter: brightness(20%);
+}
+
+.overlay {
     font-size: 40px;
     color: white;
     position: relative; 
@@ -52,42 +58,13 @@ export default {
     text-decoration: none;
 }
 
-.overlayR {
-    font-size: 40px;
-    color: white;
-    position: relative; 
-    bottom: 250px;
-    text-decoration: none;
-}
-
-.overlayL-desc{
+.overlay-desc{
     font-size: 20px;
     color: white;
     position: relative; 
     bottom: 250px;
     text-decoration: none;
-}
-
-.overlayR-desc{
-    font-size: 20px;
-    color: white;
-    position: relative; 
-    bottom: 250px;
-    text-decoration: none;
-}
-
-.overlayR:hover {
-    -webkit-transform: scale(1.2);
-    -ms-transform: scale(1.2);
-    transform: scale(1.3);
-    transition: 0.5s ease;
-}
-
-.overlayL:hover {
-    -webkit-transform: scale(1.2);
-    -ms-transform: scale(1.2);
-    transform: scale(1.3);
-    transition: 0.5s ease;
+    padding: 5px;
 }
 
 .flex-container {
@@ -98,6 +75,12 @@ export default {
     width: 600px;
     height: 400px;
     padding: 10px;
+    filter: brightness(55%);
+    -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+    -ms-transition: all 1s ease;
+    transition: all 0.6s ease;
 }
 
 .divider {
