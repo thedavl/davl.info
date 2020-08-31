299e3df30d7c114733265109e134f47eba4b1cd6
David Lee <thedavl2001@gmail.com>
2020-08-30 20:51:04 -0700

    fixing image path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
   // on Windows you might want to set publicPath: "http://127.0.0.1:8080/"
-  publicPath: "http://127.0.0.1:8080/",
+  publicPath: "",
   outputDir: "./dist/",
 
   chainWebpack: config => {
